e6251c751ccb12f137fb28fa6ce4c8ca5315731a
Kibe <kiberichard.kr@gmail.com>
2025-08-08 18:15:16 +0300

    .

--- app/admin/manage/manageplannedvisits/page.tsx
+++ app/admin/manage/manageplannedvisits/page.tsx
@@ -2,30 +2,38 @@
 
 import { upcomingVisits } from "@/app/assets/farmvisit/farmvisitassets";
 import AdminTable from "@/components/admin/admintable/AdminTable";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
-
-type VisitItem = {
-  id: string;
-  title: string;
-  slug: string;
-  status: "Pending" | "Approved";
-};
+import { FarmVisits } from "@/lib/TSInterfaces/typescriptinterface";
+import axios from "axios";
 
 export default function ManagePlannedVisitsPage() {
-  const [data, setData] = useState<VisitItem[]>(
-    upcomingVisits.map((visit) => ({
-      id: visit.id,
-      title: visit.title,
-      slug: visit.slug,
-      status: "Pending",
-    }))
-  );
+  const [data, setData] = useState<FarmVisits[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchFarmVisits = async () => {
+      try {
+        const res = await axios.get("/api/farmvisits");
+        const visitsWithId = res.data.farmvisits.map((item: FarmVisits) => ({
+          ...item,
+          id: item._id,
+        }));
+        setData(visitsWithId);
+      } catch (err) {
+        console.error("Failed to fetch visits:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFarmVisits();
+  }, []);
 
   const handleToggleStatus = (id: string) => {
     setData((prev) =>
       prev.map((item) =>
-        item.id === id
+        item._id === id
           ? {
               ...item,
               status: item.status === "Approved" ? "Pending" : "Approved",
@@ -36,7 +44,7 @@ export default function ManagePlannedVisitsPage() {
   };
 
   const handleDelete = (id: string) => {
-    setData((prev) => prev.filter((item) => item.id !== id));
+    setData((prev) => prev.filter((item) => item._id !== id));
   };
 
   const handleEdit = (id: string) => {
